03a1eb844d09d5f997e625b6be573089b6e0594b
Max Marno <jmaxmarno@gmail.com>
2017-12-02 17:06:40 -0700

    Get directions as table with distances and remove HTML tags from instructions string

--- Directions.R
+++ Directions.R
@@ -4,3 +4,9 @@ originaddress = "2785 Mitchell Dr., Bldg #9, Walnut Creek, 94598"
 
 
 route<- googleway::google_directions(units = 'imperial', key = "AIzaSyA8IkfrengHmeCnEumzbpxQjDDw2XB4wTE", origin = "Breckenridge, CO", destination = "Denver, CO", mode = 'driving')
+
+legs<- as.data.frame(route$routes$legs)
+directions.df<- as.data.frame(legs$steps)
+HTMLclean <- function(htmlString) {
+  return(gsub("<.*?>", "", htmlString))}
+directions.df$html_instructions<- sapply(directions.df$html_instructions, HTMLclean)
