1a9965d3e67a74e537c14ba21ec260c386716aef
jmaxmarno <jmaxmarno@gmail.com>
2018-02-16 12:01:33 -0700

    Update Directions.R

--- Directions.R
+++ Directions.R
@@ -3,7 +3,7 @@ require(googleway)
 originaddress = "2785 Mitchell Dr., Bldg #9, Walnut Creek, 94598"
 
 
-route<- googleway::google_directions(units = 'imperial', key = "AIzaSyA8IkfrengHmeCnEumzbpxQjDDw2XB4wTE", origin = "Breckenridge, CO", destination = "Denver, CO", mode = 'driving')
+route<- googleway::google_directions(units = 'imperial', key = "YourAPIKey", origin = "Breckenridge, CO", destination = "Denver, CO", mode = 'driving')
 
 legs<- as.data.frame(route$routes$legs)
 directions.df<- as.data.frame(legs$steps)
